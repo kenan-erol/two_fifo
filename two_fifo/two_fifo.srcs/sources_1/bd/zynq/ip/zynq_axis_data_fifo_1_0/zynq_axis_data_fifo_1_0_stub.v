e6529dd3490fa6cacd3fd9512d6a78a872b406ff
Kenan Erol <kenan.erol@yale.edu>
2023-08-05 19:48:05 -0400

    fix generate bit issue

---
+++ two_fifo/two_fifo.srcs/sources_1/bd/zynq/ip/zynq_axis_data_fifo_1_0/zynq_axis_data_fifo_1_0_stub.v
@@ -0,0 +1,37 @@
+// Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
+// Date        : Sat Aug  5 19:26:33 2023
+// Host        : caslab-wkst6 running 64-bit Ubuntu 18.04.6 LTS
+// Command     : write_verilog -force -mode synth_stub -rename_top zynq_axis_data_fifo_1_0 -prefix
+//               zynq_axis_data_fifo_1_0_ zynq_axis_data_fifo_0_0_stub.v
+// Design      : zynq_axis_data_fifo_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "axis_data_fifo_v1_1_18_axis_data_fifo,Vivado 2018.2" *)
+module zynq_axis_data_fifo_1_0(s_axis_aresetn, s_axis_aclk, s_axis_tvalid, 
+  s_axis_tready, s_axis_tdata, s_axis_tkeep, s_axis_tlast, m_axis_tvalid, m_axis_tready, 
+  m_axis_tdata, m_axis_tkeep, m_axis_tlast, axis_data_count, axis_wr_data_count, 
+  axis_rd_data_count)
+/* synthesis syn_black_box black_box_pad_pin="s_axis_aresetn,s_axis_aclk,s_axis_tvalid,s_axis_tready,s_axis_tdata[63:0],s_axis_tkeep[7:0],s_axis_tlast,m_axis_tvalid,m_axis_tready,m_axis_tdata[63:0],m_axis_tkeep[7:0],m_axis_tlast,axis_data_count[31:0],axis_wr_data_count[31:0],axis_rd_data_count[31:0]" */;
+  input s_axis_aresetn;
+  input s_axis_aclk;
+  input s_axis_tvalid;
+  output s_axis_tready;
+  input [63:0]s_axis_tdata;
+  input [7:0]s_axis_tkeep;
+  input s_axis_tlast;
+  output m_axis_tvalid;
+  input m_axis_tready;
+  output [63:0]m_axis_tdata;
+  output [7:0]m_axis_tkeep;
+  output m_axis_tlast;
+  output [31:0]axis_data_count;
+  output [31:0]axis_wr_data_count;
+  output [31:0]axis_rd_data_count;
+endmodule
